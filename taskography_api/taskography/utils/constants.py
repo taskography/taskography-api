eb8c5a021f5b2ec5b58c8a2dace3936a91fccd00
agiachris <christopheragia@gmail.com>
2022-06-23 12:17:03 -0700

    add official benchmark domain list

--- taskography_api/taskography/utils/constants.py
+++ taskography_api/taskography/utils/constants.py
@@ -302,3 +302,59 @@ TEXTURES = {
         'wrinkled',
     }
 }
+
+
+# PDDLGym Taskography benchmark domain names
+BENCHMARK_DOMAINS = [
+    ## Full Domains
+    # Rearrangment(k)
+    "taskographyv2tiny1",
+    "taskographyv2medium1",
+    "taskographyv2tiny2",
+    "taskographyv2medium2",
+    "taskographyv2tiny5",
+    "taskographyv2medium5",
+    "taskographyv2tiny10",
+    "taskographyv2medium10",
+    # Courier(n, k)
+    "taskographyv3tiny5bagslots5",
+    "taskographyv3medium5bagslots5",
+    "taskographyv3tiny10bagslots3",
+    "taskographyv3medium10bagslots3",
+    "taskographyv3tiny10bagslots5",
+    "taskographyv3medium10bagslots5",
+    "taskographyv3tiny10bagslots7",
+    "taskographyv3medium10bagslots7",
+    "taskographyv3tiny10bagslots10",
+    "taskographyv3medium10bagslots10",
+    # Lifted Rearrangement(k)
+    "taskographyv4tiny5",
+    "taskographyv4medium5",
+    # Lifted Courier(n, k)
+    "taskographyv5tiny5bagslots5",
+    "taskographyv5medium5bagslots5",
+    
+    ## Scrubbed Domains
+    # Rearrangement(k)
+    "taskographyv2tiny1scrub",
+    "taskographyv2medium1scrub",
+    "taskographyv2tiny2scrub",
+    "taskographyv2medium2scrub",
+    "taskographyv2tiny10scrub",
+    "taskographyv2medium10scrub",
+    # Courier(n, k)
+    "taskographyv3tiny10bagslots10scrub",
+    "taskographyv3medium10bagslots10scrub",
+    "taskographyv3tiny10bagslots3scrub",
+    "taskographyv3medium10bagslots3scrub",
+    "taskographyv3tiny10bagslots5scrub",
+    "taskographyv3medium10bagslots5scrub",
+    "taskographyv3tiny10bagslots7scrub",
+    "taskographyv3medium10bagslots7scrub",
+    # Lifted Rearrangement(k)
+    "taskographyv4tiny5scrub",
+    "taskographyv4medium5scrub",
+    # Lifted Courier(n, k)
+    "taskographyv5tiny5bagslots5scrub",
+    "taskographyv5medium5bagslots5scrub"
+]
